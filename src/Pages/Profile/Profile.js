2ca7a787bd9add0dbd56dd691721c85dc5da7c44
mohamedosama2 <latokalatohamato@gmail.com>
2021-02-24 16:59:03 +0200

    make it better 4

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,25 +1,25 @@
 import React from "react";
-import "./Profile.css";
+import  s from "./Profile.module.css";
 import add from "../../images/add.png";
 import profile from "../../images/profile.jpg";
 import line from "../../images/svg/line.svg";
 function Profile(props) {
   return (
-    <section class="profile">
+    <section class={s.profile}>
       <div class="container">
         <div class="profile-info">
-          <img class="line" src={line} alt="line" />
-          <div class="circle">
-            <img
-              class="profile-img"
+          <img class={s.line} src={line} alt="line" />
+          <div class={s.circle}>
+            <img 
+              class={s.profile_img}
               src="https://picsum.photos/220/220"
               alt="profile"
             />
-            <img class="add-img" src={add} alt="add" />
+            <img class={s.add_img} src={add} alt="add" />
           </div>
-          <div class="info">
+          <div class={s.info}>
             <h4>kyrillos hany</h4>
-            <div class="details">
+            <div class={s.details}>
               <div>
                 <span>Weight: </span>60kg
               </div>
@@ -35,17 +35,17 @@ function Profile(props) {
 
         {/* images section */}
         <hr />
-        <div className="Images">
-          <h2 className="Images__heading">
+        <div className={s.Images}>
+          <h2 className={s.Images__heading}>
             {" "}
             A comparison indicating your changes every month{" "}
           </h2>
-          <div className="Images__info">
-            <div className="Images__photo">
+          <div className={s.Images__info}>
+            <div className={s.Images__photo}>
               <img src={profile} alt="profile pic" />
             </div>
-            <div className="Images__btns">
-                <div className="Images__btns--monthes">
+            <div className={s.Images__btns}>
+                <div className={s.Images__btns_monthes} >
 
                 <button> Month 1 </button>
                 <button> Month 2 </button>
@@ -58,7 +58,7 @@ function Profile(props) {
                 <button> Month 9 </button>
                 <button> Month 10 </button>
                 </div>
-            <button className="Images__update">UPDATE </button>
+            <button className={s.Images__update}>UPDATE </button>
             </div>
           </div>
         </div>
@@ -67,22 +67,22 @@ function Profile(props) {
 
         <hr />
 
-        <div class="charts">
+        <div class={s.charts}>
           <h3>Motivation Charts</h3>
-          <div id="calendar_basic" class="chart-content"></div>
+          <div id="calendar_basic" class={s.chart_content}></div>
         </div>
 
         <hr />
 
-        <div class="cards">
-          <div class="card-container">
-            <div class="upper-container">
-              <div class="image-container">
+        <div class={s.cards}>
+          <div class={s.card_container}>
+            <div class={s.upper_container}>
+              <div class={s.image_container}>
                 <img src={profile} />
               </div>
             </div>
 
-            <div class="lower-container">
+            <div class={s.lower_container}>
               <div>
                 <h3>Alaina Wick</h3>
                 <h4>Front-end Developer</h4>
@@ -101,14 +101,14 @@ function Profile(props) {
             </div>
           </div>
 
-          <div class="card-container">
-            <div class="upper-container">
-              <div class="image-container">
+          <div class={s.card_container}>
+            <div class={s.upper_container}>
+              <div class={s.image_container}>
                 <img src={profile} />
               </div>
             </div>
 
-            <div class="lower-container">
+            <div class={s.lower_container}>
               <div>
                 <h3>Alaina Wick</h3>
                 <h4>Front-end Developer</h4>
@@ -127,14 +127,14 @@ function Profile(props) {
             </div>
           </div>
 
-          <div class="card-container">
-            <div class="upper-container">
-              <div class="image-container">
+          <div class={s.card_container}>
+            <div class={s.upper_container}>
+              <div class={s.image_container}>
                 <img src={profile} />
               </div>
             </div>
 
-            <div class="lower-container">
+            <div class={s.lower_container}>
               <div>
                 <h3>Alaina Wick</h3>
                 <h4>Front-end Developer</h4>
@@ -152,14 +152,14 @@ function Profile(props) {
               </div>
             </div>
           </div>
-          <div class="card-container">
-            <div class="upper-container">
-              <div class="image-container">
+          <div class={s.card_container}>
+            <div class={s.upper_container}>
+              <div class={s.image_container}>
                 <img src={profile} />
               </div>
             </div>
 
-            <div class="lower-container">
+            <div class={s.lower_container}>
               <div>
                 <h3>Alaina Wick</h3>
                 <h4>Front-end Developer</h4>
@@ -177,14 +177,14 @@ function Profile(props) {
               </div>
             </div>
           </div>
-          <div class="card-container">
-            <div class="upper-container">
-              <div class="image-container">
+          <div class={s.card_container}>
+            <div class={s.upper_container}>
+              <div class={s.image_container}>
                 <img src={profile} />
               </div>
             </div>
 
-            <div class="lower-container">
+            <div class={s.lower_container}>
               <div>
                 <h3>Alaina Wick</h3>
                 <h4>Front-end Developer</h4>
@@ -202,14 +202,14 @@ function Profile(props) {
               </div>
             </div>
           </div>
-          <div class="card-container">
-            <div class="upper-container">
-              <div class="image-container">
+          <div class={s.card_container}>
+            <div class={s.upper_container}>
+              <div class={s.image_container}>
                 <img src={profile} />
               </div>
             </div>
 
-            <div class="lower-container">
+            <div class={s.lower_container}>
               <div>
                 <h3>Alaina Wick</h3>
                 <h4>Front-end Developer</h4>
