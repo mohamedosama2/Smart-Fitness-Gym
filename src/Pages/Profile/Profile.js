0cdb0107dd2be6b31c77ce816279019b15dcc355
Bondok6 <kirolousbondok6@gmail.com>
2021-03-05 17:14:02 +0200

    /profile__table

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -64,7 +64,68 @@ function Profile(props) {
         </div>
 
         <hr />
+        <table className={s.table}>
+            <thead>
+              <th></th>
+              <th>saturday</th>
+              <th>sunday</th>
+              <th>monday</th>
+              <th>tuesday</th>
+              <th>wednesday</th>
+              <th>friday</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>8 Am</td>
+                <td data-label="sunday">lorem</td>
+                <td data-label="monday">lorem</td>
+                <td data-label="tuesday">lorem</td>
+                <td data-label="wednesday">lorem</td>
+                <td data-label="friday">lorem</td>
+                <td data-label="friday">lorem</td>
+              </tr>
 
+              <tr>
+                <td>11 Am</td>
+                <td data-label="sunday">lorem</td>
+                <td data-label="monday">lorem</td>
+                <td data-label="tuesday">lorem</td>
+                <td data-label="wednesday">lorem</td>
+                <td data-label="friday">lorem</td>
+                <td data-label="friday">lorem</td>
+              </tr>
+
+              <tr>
+                <td>3 Pm</td>
+                <td data-label="sunday">lorem</td>
+                <td data-label="monday">lorem</td>
+                <td data-label="tuesday">lorem</td>
+                <td data-label="wednesday">lorem</td>
+                <td data-label="friday">lorem</td>
+                <td data-label="friday">lorem</td>
+              </tr>
+
+              <tr>
+                <td>5 Pm</td>
+                <td data-label="sunday">lorem</td>
+                <td data-label="monday">lorem</td>
+                <td data-label="Age">lorem</td>
+                <td data-label="wednesday">lorem</td>
+                <td data-label="friday">lorem</td>
+                <td data-label="friday">lorem</td>
+              </tr>
+
+              <tr>
+                <td>7 Pm</td>
+                <td data-label="sunday">lorem</td>
+                <td data-label="monday">lorem</td>
+                <td data-label="Age">lorem</td>
+                <td data-label="wednesday">lorem</td>
+                <td data-label="friday">lorem</td>
+                <td data-label="friday">lorem</td>
+              </tr>
+            </tbody>
+          </table>
         <hr />
 
         <div className={s.charts}>
